c59a65499b23efd71f787a725bf79c45e11ce9ca
zachmort <zach.mortenson7@gmail.com>
2023-07-09 15:16:34 -0400

    adding code for pd.df merge

---
+++ to_dos.py
@@ -0,0 +1,7 @@
+## To dos:
+# hit spotify api end point for audio track features:
+# https://developer.spotify.com/documentation/web-api/reference/get-audio-features
+
+# Create a db for track details + track audio details
+
+# Analyze and visualise data 
